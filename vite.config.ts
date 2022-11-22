d4a7a61b0a31b6e862bf7ba95e112ef968745ce5
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2022-11-22 16:09:43 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+        '/api': {
+            target: 'https://hbv1-db.herokuapp.com',
+            changeOrigin: true,
+            secure: false
+        },
+        '/oauth2': {
+            target: 'https://hbv1-db.herokuapp.com',
+            changeOrigin: true,
+            secure: false
+        }
+    }
+},
 })
